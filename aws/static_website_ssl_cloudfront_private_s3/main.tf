f526a6dc035f2e4f291861def90cb18bf098d9b9
Sudharsan SundaraBabu <sudharsan.s@gmail.com>
2023-01-27 08:18:01 +0000

    feat: unblock public acl/policy

--- aws/static_website_ssl_cloudfront_private_s3/main.tf
+++ aws/static_website_ssl_cloudfront_private_s3/main.tf
@@ -24,8 +24,8 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_public_access_block" "website_bucket" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls   = false
+  block_public_policy = false
 }
 
 data "aws_iam_policy_document" "s3_policy_document" {
